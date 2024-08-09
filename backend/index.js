96946f5cc8cbc947334806bd5d63903f7a77ff86
Oren <orenbaranovsky@gmail.com>
2024-08-09 12:59:18 +0300

    anna provided backedn

--- backend/index.js
+++ backend/index.js
@@ -3,38 +3,238 @@ const app = express();
 const port = process.env.PORT || 3000;
 const {db} = require('./firebase.js')
 
+
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/backend_db_test', async (req, res) => {
+  res.send('HELLO TESTER, CHECK YOUR DB');
   const peopleRef = db.collection('people').doc('OREN :)')
   const res2 = await peopleRef.set({
     ["backend_db_test"]: "Added this value yey!! from vercel!!!"
   })
-  res.send(res2);
+});
+
+// Adding users manually - change if needed!
+app.get('/add_user', async (req, res) => {
+  console.log('I am in add_person');
+  try {
+    /*const peopleRef = db.collection('users').doc('337889133');
+    await peopleRef.set({
+      name: "Anna",
+      surname: "Garmash",
+      friends: ["311170450", "319003943"]
+    });
+    const peopleRef2 = db.collection('users').doc('311170450');
+    await peopleRef2.set({
+      name: "Oren",
+      surname: "Baranovsky",
+      friends: ["337889125", "319003943","211567342"]
+    });
+    const peopleRef3 = db.collection('users').doc('319003943');
+    await peopleRef3.set({
+      name: "Lior",
+      surname: "Panikashvili",
+      friends: ["337889125", "311170450","211567342"]
+    });*/
+    const peopleRef4 = db.collection('users').doc('333333333');
+    await peopleRef4.set({
+      name: "Kate",
+      surname: "Garmash",
+      friends: ["1111", "111"]
+    });
+    res.send('Person added to Firestore');
+  }catch (error) {
+    console.error("Error adding person: ", error);
+  }
+});
+
+// Endpoint for adding new habit
+// endpoint should get user's id and habit name
+app.get('/add_habit', async (req, res) => {
+  console.log('I am in add_habit');
+
+  const {id, habitName} = req.query;
+  //const id = "337889125";
+  //const habitName = "Climbing";
+
+  if (!id || !habitName) {
+    return res.status(400).send('ID and habit name are required');
+  }
+
+  try {
+    // Reference to the user's document
+    const userRef = db.collection('users_habits').doc(id);
+    const userDoc = await userRef.get();
+
+    // Create a structure for the habit with an empty list of events
+    const newHabit = {
+      name: habitName,
+      events: [] // Empty list of habit events
+    };
+
+    // Check if the user document exists
+    if (userDoc.exists) {
+      // User exists, so update their document with the new habit
+      await userRef.update({
+        [habitName]: newHabit
+      });
+    } else {
+      // User doesn't exist, create a new document with the habit
+      await userRef.set({
+        [habitName]: newHabit
+      });
+    }
+
+    res.status(200).send('Habit added successfully');
+  } catch (error) {
+    console.error('Error adding habit:', error);
+    res.status(500).send('Error adding habit');
+  }
+});
+
+// Endpoint adding event for specific habig
+// endpoint should get user's id, habit name and event(date)
+app.get('/add_event_to_habit', async (req, res) => {
+  console.log('I am in add_event');
+
+  const { id, habitName, event } = req.query;
+
+  if (!id || !habitName || !event) {
+    return res.status(400).send('ID, habit name, and event are required');
+  }
+
+  try {
+    // Reference to the user's document
+    const userRef = db.collection('users_habits').doc(id);
+    const userDoc = await userRef.get();
+
+    if (!userDoc.exists) {
+      return res.status(404).send('User or habit not found');
+    }
+
+    const habitData = userDoc.data()[habitName];
+
+    if (!habitData) {
+      return res.status(404).send('Habit not found');
+    }
+
+    // Add the new event to the habit's events array
+    habitData.events.push(event);
+
+    // Update the habit with the new event in Firestore
+    await userRef.update({
+      [habitName]: habitData
+    });
+
+    res.status(200).send('Event added successfully');
+  } catch (error) {
+    console.error('Error adding event:', error);
+    res.status(500).send('Error adding event');
+  }
 });
 
 
-app.get('/backend_db_test2', async (req, res) => {
-  const peopleRef = db.collection('people').doc('OREN :)')
-  const res2 = await peopleRef.set({
-    ["backend_db_test"]: "Added this value yey!! from vercel!!! 222222"
-  })
-  res.send(res2);
+// Endpoint for getting user's personal data
+// endpoint should get user's id
+app.get('/get_user_personal_data', async (req, res) => {
+  console.log('I am in get_user_personal_data');
+  //id = "337889133";
+  const {id} = req.query;
+  try {
+    if(!id) {
+      return res.status(400).send('User ID is required');
+    }
+
+    const userRef = db.collection('users').doc(id);
+    const userDoc = await userRef.get();
+    
+    if (!userDoc.exists) {
+      res.status(404).send('No such user found!');
+    } else {
+      console.log(userDoc.data());
+      res.json(userDoc.data());
+    }
+  }catch (error) {
+    console.error("Error getting user: ", error);
+  }
+});
+
+// Endpoint for getting user's habit list:
+// endpoint should get user's id
+app.get('/get_user_habits', async (req, res) => {
+  console.log('I am in get_user_habits');
+  const id = req.query.id;
+  //const id = "337889125";
+
+  if (!id) {
+    return res.status(400).send('User ID is required');
+  }
+
+  try {
+    const userHabitsRef = db.collection('users_habits').doc(id);
+    const userHabitsDoc = await userHabitsRef.get();
+
+    if (!userHabitsDoc.exists) {
+      return res.status(404).send('No habits found for this user');
+    }
+
+    const userHabitsData = userHabitsDoc.data();
+
+    // Extract habit names from the document's fields
+    const habitList = Object.keys(userHabitsData);
+
+    console.log('User habits:', habitList);
+    res.json({ habits: habitList });
+  } catch (error) {
+    console.error("Error retrieving user's habits: ", error);
+    res.status(500).send('Error retrieving habits');
+  }
 });
 
 
-app.get('/backend_db_test3', async (req, res) => {
+// Endpoint for getting user's specific habit data:
+// endpoint should get: user's id, habit name
+app.get('/get_specific_habit', async (req, res) => {
+  console.log('I am in get_specific_habit');
   
-  const peopleRef = db.collection('people').doc('Anna')
-  const res2 = await peopleRef.set({
-    ["backend_db_test"]: "anna valuie 3"
-  })
-  res.send(res2);
-});
+  const id = req.query.id;
+  const habitName = req.query.habitName;
 
+  //const id = "337889125";
+  //const habitName = "Swimming";
+
+  if (!id || !habitName) {
+    return res.status(400).send('ID and habit name are required');
+  }
+
+  try {
+    // Reference to the user's habit document
+    const habitRef = db.collection('users_habits').doc(id);
+    const habitDoc = await habitRef.get();
+
+    if (!habitDoc.exists) {
+      return res.status(404).send('No habit data found for this user');
+    }
+
+    const habitData = habitDoc.data();
+
+    // Check if the habit exists
+    if (!habitData[habitName]) {
+      return res.status(404).send('Habit not found for this user');
+    }
+
+    // Retrieve the specific habit data
+    const habit = habitData[habitName];
+    console.log(habit);
+    res.json(habit);
+  } catch (error) {
+    console.error("Error retrieving habit data: ", error);
+    res.status(500).send('Error retrieving habit data');
+  }
+});
 
 // Start the Express server
 app.listen(port, () => {
