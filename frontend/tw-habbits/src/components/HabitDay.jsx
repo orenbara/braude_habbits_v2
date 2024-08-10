9b953f10e3482852ae747556953806f70563a3db
Oren <orenbaranovsky@gmail.com>
2024-08-10 17:42:38 +0300

    darkmode fix by anna

--- frontend/tw-habbits/src/components/HabitDay.jsx
+++ frontend/tw-habbits/src/components/HabitDay.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 const HabitDay = ({day, color, active,switchActive}) => {
+
     return (
         <div className="flex flex-col items-center text-lg">
             <h4 className="text-gray-400 dark:text-sky-100 mt-5 mb-2">{day?.title}</h4>
-            <button onClick={switchActive} className={`rounded-full w-10 h-10 font-bold ${active ? 'text-white dark:text-black' : 'text-black dark:text-white'}`}
-                    style={active ? {background: color} : {}}>
+            <button
+                onClick={switchActive}
+                className={`rounded-full w-10 h-10 font-bold ${active ? 'text-white dark:text-white' : 'text-black dark:text-white'}`}
+                style={active ? {background: color} : {}}
+            >
                 {day?.date}
             </button>
         </div>
