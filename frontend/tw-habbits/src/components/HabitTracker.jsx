3b42a4ecd5c261fa85ed98c527b57c6ff13f83b6
Oren <orenbaranovsky@gmail.com>
2024-08-14 19:30:25 +0300

    anna fix darkmode

--- frontend/tw-habbits/src/components/HabitTracker.jsx
+++ frontend/tw-habbits/src/components/HabitTracker.jsx
@@ -179,8 +179,7 @@ const HabitTracker = () => {
               <div className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-48 bg-white border rounded shadow-lg z-10">
                 <ul>
                   {habitList.map((habit) => (
-                    <li key={habit.key} className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                      onClick={() => handleHabitSelect(habit)}>
+                    <li key={habit.key} className="px-4 py-2 hover:bg-gray-100 cursor-pointer dark:bg-slate-700 dark:text-white"                      onClick={() => handleHabitSelect(habit)}>
                       {habit.title}
                     </li>
                   ))}
